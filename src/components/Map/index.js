1586c46afae482a52f297792701e19ca8499355c
gabrieldaghetti <gabrieljrld1@gmail.com>
2020-07-13 12:08:40 -0300

    finalizando

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,16 +1,40 @@
-import React, { useEffect, useState } from "react";
-import MapView from "react-native-maps";
+import React, { useEffect, useState, useRef } from "react";
+import MapView, { Marker } from "react-native-maps";
 import Geolocation from "@react-native-community/geolocation";
-import { View } from "react-native";
+import { View, Image } from "react-native";
 import Search from "../Search";
+import Directions from "../Directions";
+import { getPixelSize } from "../utils";
+import markerImage from "../../assets/marker.png";
+import backImage from "../../assets/back.png";
+import {
+  LocationBox,
+  LocationText,
+  LocationTimeBox,
+  LocationTimeText,
+  LocationTimeTextSmall,
+  Back,
+} from "./styles";
+import Geocoder from "react-native-geocoding";
+import Details from "../Details";
+
+Geocoder.init("AIzaSyBgzca1msPvKdahMpf43PdTv4J_Xn8d728");
 
 function Map() {
   const [region, setRegion] = useState(null);
+  const [destination, setDestination] = useState(null);
+  const [duration, setDuration] = useState(null);
+  const [location, setLocation] = useState(null);
+  const mapViewRef = useRef();
 
   useEffect(() => {
     async function getLocation() {
       Geolocation.getCurrentPosition(
-        ({ coords: { latitude, longitude } }) => {
+        async ({ coords: { latitude, longitude } }) => {
+          const response = await Geocoder.from({ latitude, longitude });
+          const address = response.results[0].formatted_address;
+          const location = address.substring(0, address.indexOf(","));
+          setLocation(location);
           setRegion({
             latitude,
             longitude,
@@ -27,8 +51,22 @@ function Map() {
       );
     }
     getLocation();
-  });
+  }, []);
 
+  function handleLocationSelected(data, { geometry }) {
+    const {
+      location: { lat: latitude, lng: longitude },
+    } = geometry;
+    setDestination({
+      latitude,
+      longitude,
+      title: data.structured_formatting.main_text,
+    });
+  }
+
+  function handleBack() {
+    setDestination(null);
+  }
   return (
     <View style={{ flex: 1 }}>
       <MapView
@@ -36,10 +74,61 @@ function Map() {
         initialRegion={region}
         showsUserLocation
         loadingEnabled
+        ref={mapViewRef}
       >
-        {/* <Marker coordinate={{ latitude: 51.5078788, longitude: -0.0877321 }} /> */}
+        {destination && (
+          <React.Fragment>
+            <Directions
+              origin={region}
+              destination={destination}
+              onReady={(result) => {
+                setDuration(Math.floor(result.duration));
+                if (mapViewRef) {
+                  mapViewRef.current.fitToCoordinates(result.coordinates, {
+                    edgePadding: {
+                      top: getPixelSize(50),
+                      bottom: getPixelSize(350),
+                      right: getPixelSize(50),
+                      left: getPixelSize(50),
+                    },
+                  });
+                }
+              }}
+            />
+            <Marker
+              coordinate={destination}
+              anchor={{ x: 0, y: 0 }}
+              image={markerImage}
+            >
+              <LocationBox>
+                <LocationText>{destination.title}</LocationText>
+              </LocationBox>
+            </Marker>
+
+            <Marker coordinate={region} anchor={{ x: 0, y: 0 }}>
+              <LocationBox>
+                <LocationTimeBox>
+                  <LocationTimeText>{duration}</LocationTimeText>
+                  <LocationTimeTextSmall>MIN</LocationTimeTextSmall>
+                </LocationTimeBox>
+
+                <LocationText>{location}</LocationText>
+              </LocationBox>
+            </Marker>
+          </React.Fragment>
+        )}
       </MapView>
-      <Search />
+
+      {destination ? (
+        <React.Fragment>
+          <Back onPress={handleBack}>
+            <Image source={backImage} />
+          </Back>
+          <Details />
+        </React.Fragment>
+      ) : (
+        <Search onLocationSelected={handleLocationSelected} />
+      )}
     </View>
   );
 }
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { View, Platform } from "react-native";
 
 // import { Container } from './styles';
 
-function Search() {
+function Search(props) {
+  const { onLocationSelected } = props;
+  const [searchFocused, setSearchFocused] = useState(false);
   return (
     <GooglePlacesAutocomplete
       placeholder="Para onde?"
       placeholderTextColor="#333"
-      onPress={() => {}}
+      onPress={onLocationSelected}
       query={{
         key: "AIzaSyBgzca1msPvKdahMpf43PdTv4J_Xn8d728",
-        language: "pt-BR",
+        language: "pt",
       }}
-      textInputProps={{ autoCapitalize: "none", autoCorrect: false }}
+      textInputProps={{
+        autoCapitalize: "none",
+        autoCorrect: false,
+        onFocus: () => setSearchFocused(true),
+        onBlur: () => setSearchFocused(false),
+      }}
+      listViewDisplayed={searchFocused}
       fetchDetails
       enablePoweredByContainer={false}
       styles={{
@@ -23,11 +31,53 @@ function Search() {
           top: Platform.select({ ios: 60, android: 40 }),
           width: "100%",
         },
-        textInput: {},
-        textInputContainer: {},
-        listView: {},
-        description: {},
-        row: {},
+        textInputContainer: {
+          flex: 1,
+          backgroundColor: "transparent",
+          height: 54,
+          marginHorizontal: 20,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+        },
+        textInput: {
+          height: 54,
+          margin: 0,
+          borderRadius: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingLeft: 20,
+          paddingRight: 20,
+          marginTop: 0,
+          marginLeft: 0,
+          marginRight: 0,
+          elevation: 5,
+          shadowColor: "#000",
+          shadowOpacity: 0.1,
+          shadowOffset: { x: 0, y: 0 },
+          shadowRadius: 15,
+          borderWidth: 1,
+          borderColor: "#DDD",
+          fontSize: 18,
+        },
+        listView: {
+          borderWidth: 1,
+          borderColor: "#DDD",
+          backgroundColor: "#FFF",
+          marginHorizontal: 20,
+          elevation: 5,
+          shadowColor: "#000",
+          shadowOpacity: 0.1,
+          shadowOffset: { x: 0, y: 0 },
+          shadowRadius: 15,
+          marginTop: 10,
+        },
+        description: {
+          fontSize: 16,
+        },
+        row: {
+          padding: 20,
+          height: 58,
+        },
       }}
     />
   );
